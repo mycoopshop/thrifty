d798550932633e457e6997cb667d5fba6841d608
Andy Bettisworth <andy@accreu.com>
2017-11-04 18:30:05 -0500

    add some comments

--- config/app.js
+++ config/app.js
@@ -1,19 +1,50 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
 const express = require('express')
-const app = express()
 const path = require('path')
 
-let port = 53001
+/**
+ * Initialize app
+ */
+
+const app = express()
+
+/**
+ * Constants
+ */
+
+const port = 53001
+
+/**
+ * Middleware
+ */
 
 app.use(express.static(path.join(__dirname, '/../app/views')))
 
+/**
+ * Routes
+ */
+
 app.get('/', (req, res) => {
-  res.redirect('/hello.html')
+  res.render('hello')
 })
 
-if (!module.parent) {
+/**
+ * Start server
+ */
+
+if (module === require.main) {
   app.listen(port, () => {
     console.log(`Running express.js app on port ${port}`)
   })
 }
+
+/**
+ * Export app
+ */
 
 module.exports = app
