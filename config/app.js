c677d57518a2f40b73353b15bc14e385b6451b8f
Andy Bettisworth <andy@accreu.com>
2017-11-04 18:46:42 -0500

    add deps

--- config/app.js
+++ config/app.js
@@ -6,6 +6,7 @@
 
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs')
 
 /**
  * Initialize app
@@ -17,7 +18,46 @@ const app = express()
  * Constants
  */
 
+const title = "Thrifty"
 const port = 53001
+const base = path.join(__dirname, '..')
+const env = process.env.NODE_ENV || 'development'
+const views = path.join(base, '/app/views')
+
+/**
+ * Locals
+ */
+
+app.locals.title = title
+app.locals.port = port
+app.locals.base = base
+app.locals.env = env
+app.locals.views = views
+
+/**
+ * Settings
+ */
+
+app.set('env', env)
+app.disable('x-powered-by')
+
+/**
+ * View engine
+ */
+
+app.engine('html.ejs', ejs.renderFile)
+app.set('view engine', '.html.ejs')
+app.set('views', views)
+
+/**
+ * Static assets
+ */
+
+app.use('/assets', express.static(base + '/node_modules/jquery/dist'))
+app.use('/assets', express.static(base + '/node_modules/popper.js/dist/umd'))
+app.use('/assets', express.static(base + '/node_modules/font-awesome'))
+app.use('/assets', express.static(base + '/node_modules/bootstrap/dist'))
+app.use('/assets/js', express.static(base + '/node_modules/turbolinks/dist'))
 
 /**
  * Middleware
