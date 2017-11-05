6fb6440d6831be491f3fa63000563b2ea2393417
Andy Bettisworth <andy@accreu.com>
2017-11-05 15:09:45 -0600

    merge into a users table

--- db/migrations/20171105002650_create_table_balance.js
+++ db/migrations/20171105002650_create_table_balance.js
@@ -3,11 +3,12 @@
 module.exports = (app) => {
   app.locals.db.serialize(() => {
     app.locals.db.run(`
-CREATE TABLE balance (
+CREATE TABLE users (
   id integer UNIQUE,
-  amount integer DEFAULT 0
+  balance integer DEFAULT 0,
+  currency integer DEFAULT 0
 );`)
 
-    app.locals.db.run('INSERT INTO balance (id, amount) VALUES (0, 0);')
+    app.locals.db.run('INSERT INTO users (id, balance, currency) VALUES (0, 0, 0);')
   })
 }
