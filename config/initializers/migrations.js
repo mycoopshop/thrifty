efb0885baf4ad771e488ee7854226672c779d1f8
Andy Bettisworth <andy@accreu.com>
2018-05-04 06:54:14 -0500

    deps: add schema-migrations

--- config/initializers/migrations.js
+++ config/initializers/migrations.js
@@ -1,32 +1,30 @@
 "use strict"
 
-const path = require("path")
-const fs = require("fs")
+module.exports = async (app) => {
+  /**
+   * Dependencies
+   */
 
-module.exports = (app) => {
-  const create_table_schema_migrations = `
-CREATE TABLE IF NOT EXISTS schema_migrations (
-  version text UNIQUE ON CONFLICT IGNORE
-);
-  `
+  const SchemaMigrations = require("schema-migrations")
 
-  app.locals.db.exec(create_table_schema_migrations, () => {
-    app.locals.db.all("SELECT version FROM schema_migrations;", (err, rows) => {
-      let migrations = []
-      let version
+  /**
+   * Constants
+   */
 
-      for (let i=0; i < rows.length; i++) { migrations[i] = rows[i].version }
+  const base = app.locals.base
+  const db = app.locals.db
 
-      let files = fs.readdirSync(path.join(app.locals.base, "/db/migrations"))
-      for (let i=0; i < files.length; i++) {
-        if (/\d+/.test(files[i])) {
-          version = files[i].match(/\d+/)[0]
-          if (!migrations.includes(version)) {
-            require(path.join(app.locals.base, "/db/migrations/", files[i]))(app)
-            app.locals.db.run("INSERT INTO schema_migrations VALUES (?)", version)
-          }
-        }
-      }
-    })
-  })
+  /**
+   * Run any pending migrations (production)
+   */
+
+  let schema_migrations = new SchemaMigrations(base, db, false)
+
+  try {
+    await schema_migrations.run()
+  } catch(err) {
+    console.error(err)
+  }
+
+  return true
 }
